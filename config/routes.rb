dfaa05a446f8bb6bffdfefb5e06f577bbf46691c
07naoki07 <naokiti.0205.22@gmail.com>
2022-02-12 16:27:28 +0000

    add update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
+  get 'top' => "homes#top"
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
   get 'books/edit'
-  get '/top' => "homes#top"
+  get 'books/:id' =>'books#show',as:'list'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
